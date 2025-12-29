e0fda8e9bd978ebfb6917181607ba1713ff3d22a
jiangliangtong <jiangliangtong@gmail.com>
2025-12-29 12:22:22 +0800

    feat: init

---
+++ src/main/java/com/java/poker/PokerApplication.java
@@ -0,0 +1,17 @@
+package com.java.poker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author whoami
+ * Create by 2025/12/26
+ */
+@SpringBootApplication
+public class PokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokerApplication.class, args);
+    }
+
+}
